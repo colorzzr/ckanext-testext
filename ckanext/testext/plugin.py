1347888a4bcbd5c2a176ee6b41a68bee79d0f6ef
colorzzr <colorzzr@gmail.com>
2019-05-10 17:02:59 -0400

    update the ckan.model

--- ckanext/testext/plugin.py
+++ ckanext/testext/plugin.py
@@ -4,7 +4,7 @@ from ckanext.testext.interfaces import testinterface
 # from ckanext.testext.controller import TestController
 from ckan.controllers.package import PackageController
 from ckan.lib.base import BaseController
-
+import ckan.model as model
 
 class TestextPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
@@ -45,7 +45,27 @@ class TestextPlugin(plugins.SingletonPlugin):
 
 
 class TestPackageController(BaseController):
-
+	# All the attribute in model
+	# ['rating', 'ResourceRevision', 'OrderedDict', 'SQLAlchemySession', 'revision_table', 'group_extra', 
+	# 'engine_is_pg', 'engine_is_sqlite', 'system_info_revision_table', 'MemberRevision', 'Revision', 'package_tag_table', 
+	# 'group', 'package_revision_table', 'system_info', 'PackageExtra', 'PackageTag', 'MAX_TAG_LENGTH', 'activity_detail_table', 
+	# 'GroupExtraRevision', 'misc', 'TaskStatus', 'repo', 'package_relationship', 'SystemInfoRevision', 'datetime', 'Rating', 
+	# 'resource', 'DictProxy', 'member_revision_table', 'Tag', 'DomainObject', 'activity', 'VOCABULARY_NAME_MIN_LENGTH', 'PackageRelationship', 
+	# 'vdm', 'is_id', 'PACKAGE_NAME_MAX_LENGTH', 'System', 'State', 'meta', 'MIN_TAG_LENGTH', 'group_table', 'PACKAGE_NAME_MIN_LENGTH', 
+	# 'warnings', 'package_table', 're', 'dashboard', 'core', 'MAX_RATING', 'init_model', 'package_relationship_revision_table', 
+	# 'DomainObjectOperation', 'get_system_info', 'logging', 'Repository', 'license', 'package', 'delete_system_info', 'TrackingSummary', 
+	# 'set_system_info', 'extra_revision_table', 'Session', 'task_status', 'MIN_RATING', 'Group', 'version_table', 'Member', 'follower', 
+	# 'PACKAGE_VERSION_MAX_LENGTH', 'VOCABULARY_NAME_MAX_LENGTH', 'GroupRevision', 'task_status_table', 'ckan', 'log', 'Vocabulary', 
+	# 'resource_view', 'UserFollowingGroup', 'User', 'package_extra', 'revision_as_dict', 'text_type', 'domain_object', 'user_table', 
+	# 'Dashboard', 'tracking_summary_table', 'resource_view_table', 'extension', 'package_extra_table', 'UserFollowingUser', 'term_translation_table', 
+	# 'ResourceView', 'PackageTagRevision', 'group_revision_table', 'group_extra_table', 'SystemInfo', 'PackageExtraRevision', 
+	# 'package_relationship_table', 'tag', 'tracking_raw_table', 'resource_revision_table', 'modification', 'sqav', 'ActivityDetail', 
+	# 'MetaData', 'Resource', 'vocabulary', 'Package', 'term_translation', 'member_table', 'package_tag_revision_table', 'user', 'Activity', 
+	# 'PackageRevision', 'types', 'activity_table', 'system_info_table', 'tracking', 'tag_table', 'GroupExtra', 'resource_table', 'Table', 
+	# 'UserFollowingDataset']
     def read(self, id):
-    	print("test")
+    	test = [x for x in model.user.datetime.time.__dict__.keys() if not x.startswith('_')]
+    	# print([x for x in model.user.datetime.__dict__.keys() if not x.startswith('_')])
+    	print(model.user.datetime.time)
+    	print(test)
         return 'foobar'
